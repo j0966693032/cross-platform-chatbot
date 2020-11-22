11d8714db369227716837e013bb010453d07a26c
j0966693032 <j0966693032@gmail.com>
2020-11-22 22:21:01 +0800

    Change my client_id

--- src/handler.js
+++ src/handler.js
@@ -42,16 +42,14 @@ module.exports = class RankFollow{
     toLineMessage() {    
         let template = this.data.events.slice(0, 10).map(el => {
             var url = encodeURI(el.url);
-            //return {
+            return {
                 imageUrl: 'https://kma.kkbox.com/charts/assets/images/logo.svg?id=e41750806e78fa673556/600x600.jpg',
                 action: {
                     type: 'uri',
-                    uri: 'https://kma.kkbox.com/charts/daily/song?terr=tw&lang=tc',
-                   // uri: (el.url == '') ? 'https://kma.kkbox.com/charts/daily/song/' : `${url}`,
-
                     label: `${el.title}`.slice(0, 12),
+                    uri: 'https://kma.kkbox.com/charts/daily/song?terr=tw&lang=tc',
                 }
-          //  }
+            }
         });
     }
 }
